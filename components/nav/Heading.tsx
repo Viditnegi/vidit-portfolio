37794f11097c46d6b7fb079ddcbdb7976b5b4618
Viditnegi <viditsn@gmail.com>
2026-05-18 12:07:07 +0000

    Deleted resume

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -8,7 +8,7 @@ export const Heading = () => {
       <MyLinks />
       {/* <div className={styles.headingButtons}> */}
 
-      <OutlineButton onClick={() => window.open("/Vidit_Singh_Negi_Resume.pdf")}>
+      <OutlineButton onClick={() => window.open("/Vidit_Singh_Negi.pdf")}>
         My resume
       </OutlineButton>
       {/* </div> */}
